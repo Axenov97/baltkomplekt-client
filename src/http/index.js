4c6a70722483b130da769bf28ed7a857ae8434fc
Axenov97 <dimka180497@mail.ru>
2021-12-21 13:53:52 +0300

    process.env

--- src/http/index.js
+++ src/http/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const $host = axios.create({
-    baseURL: 'http://root@vm671464.vps.masterhost.tech:5000/'
+    baseURL: process.env.REACT_APP_API_URL
 })
 
 const $authHost = axios.create({
-    baseURL: "http://root@vm671464.vps.masterhost.tech:5000/"
+    baseURL: process.env.REACT_APP_API_URL
 })
 
 const authInterceptor = config => {
